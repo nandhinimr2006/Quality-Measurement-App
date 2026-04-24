ac464877da68497bb1527fd826268535d98c76b5
nandhinimr2006 <nandhinimr2006@gmail.com>
2026-04-24 09:22:47 +0530

    UC2: Feet and Inches measurement equality

--- app/src/QuantityMeasurementApp.java
+++ app/src/QuantityMeasurementApp.java
@@ -1,19 +1,48 @@
 public class QuantityMeasurementApp {
-    public static boolean compareFeet(double v, double v1) {
-        return false;
-    }
 
-    public static boolean compareInches(double v, double v1) {
-        return false;
-    }
+    public enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0);
 
-    public static class Feet {
-        public Feet(double v) {
+        private final double toFeetFactor;
+
+        LengthUnit(double toFeetFactor) {
+            this.toFeetFactor = toFeetFactor;
+        }
+
+        public double toFeet(double value) {
+            return value * toFeetFactor;
         }
     }
 
-    public static class Inches {
-        public Inches(double v) {
+    public static class Quantity {
+        private final double value;
+        private final LengthUnit unit;
+
+        public Quantity(double value, LengthUnit unit) {
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
+            this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null) return false;
+
+            if (getClass() != obj.getClass()) return false;
+
+            Quantity other = (Quantity) obj;
+
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
         }
     }
 }
