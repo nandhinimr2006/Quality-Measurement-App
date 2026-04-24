f4906fbc93de6a5077f30cbccadb2b3bd69cbd29
nandhinimr2006 <nandhinimr2006@gmail.com>
2026-04-24 09:41:42 +0530

    UC5: Unit-to-Unit Conversion (Same Measurement Type)

--- app/src/QuantityMeasurementApp.java
+++ app/src/QuantityMeasurementApp.java
@@ -4,7 +4,7 @@ public class QuantityMeasurementApp {
         FEET(1.0),
         INCH(1.0 / 12.0),
         YARD(3.0),
-        CM(0.0328084);
+        CM(0.0328084); 
 
         private final double toFeetFactor;
 
